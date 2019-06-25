268d425325408bc1195a7115cfba857cba19b88a
Imran Ahmed <imran.ahmad@aqtsoft.com>
2019-06-25 20:44:27 +0500

    CRUD for Question and Resource transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,12 @@ use App\User;
 
 class Question extends Model
 {
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    protected $guarded = [];
+
     public function replies(){
         return $this->hasMany(Reply::class);
     }
@@ -17,5 +23,9 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
     }
 }
