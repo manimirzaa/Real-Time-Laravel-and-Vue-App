f7576fa6a5002228f82ffe235cbaa848543521aa
Imran Ahmed <imran.ahmad@aqtsoft.com>
2019-06-28 21:16:55 +0500

    Installation of Vue Router

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <footercomponent></footercomponent>
+    </div>    
+</template>
+
+<script>
+import toolbar from './Toolbar';
+import footercomponent from './FooterComponent';
+
+export default {
+  components:{toolbar, footercomponent}
+}
+</script>
+
+<style>
+
+</style>
